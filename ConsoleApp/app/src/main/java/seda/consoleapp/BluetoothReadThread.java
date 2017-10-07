9d13e75ec9d16c3964b46dfe50fcb5695f7bb377
Victor San Kho Lin <san.lin@unimelb.edu.au>
2017-10-07 22:46:35 +1100

    fix issue #15

--- ConsoleApp/app/src/main/java/seda/consoleapp/BluetoothReadThread.java
+++ ConsoleApp/app/src/main/java/seda/consoleapp/BluetoothReadThread.java
@@ -7,52 +7,44 @@ import android.widget.Toast;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-import static seda.consoleapp.MainActivity.TAG;
-
 /**
  * Created by liubingfeng on 7/10/2017.
  */
 
-public class BluetoothReadThread extends Thread
-{
+public class BluetoothReadThread extends Thread {
+
+    private static final String TAG = "BluetoothReadThread";
+
     private AppCompatActivity activity;
     BufferedReader in;
 
-    BluetoothReadThread(AppCompatActivity activity, BufferedReader in)
-    {
+    BluetoothReadThread(AppCompatActivity activity, BufferedReader in) {
         this.activity = activity;
         this.in = in;
     }
 
     @Override
-    public void run()
-    {
+    public void run() {
         String inputLine = null;
-        while (true)
-        {
+        while (true) {
             Log.wtf(TAG, "start reading");
-            try
-            {
+            try {
                 inputLine = in.readLine();
 
                 Log.wtf(TAG, "from server -> " + inputLine);
 
-    //                    displayBluetoothTextView.setText(inputLine);
+                //                    displayBluetoothTextView.setText(inputLine);
 
                 //this code is ugly will refactor it later. just want to show msg on the UI
                 final String finalInputLine = inputLine;
-                activity.runOnUiThread(new Runnable()
-                {
+                activity.runOnUiThread(new Runnable() {
                     @Override
-                    public void run()
-                    {
+                    public void run() {
                         Toast.makeText(activity.getApplicationContext(), "From server -> " + finalInputLine, Toast.LENGTH_LONG).show();
                     }
                 });
 
-            }
-            catch (IOException e)
-            {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
