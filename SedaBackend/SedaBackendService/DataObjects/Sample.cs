e80dcf6cbd7c6af03abf6581ef8b205548330e08
victorskl <victorlin21@gmail.com>
2017-10-09 06:41:54 +1100

    Backend documentation for Sample.cs

--- SedaBackend/SedaBackendService/DataObjects/Sample.cs
+++ SedaBackend/SedaBackendService/DataObjects/Sample.cs
@@ -1,5 +1,18 @@
 ﻿using Microsoft.Azure.Mobile.Server;
-
+/**
+ * SEDA sampling data
+ * Each sample has startTime and endTime to denote the particular type of
+ * sensor data sample recording. 
+ * 
+ * The sample type define, the type of the trigger event using one or 
+ * more sensors at a particular event occur at driving condition.
+ * Currently map to:
+ * 1 = HEAD_CHECK_NEG_CNT
+ * 2 = HEAD_CHECK_POS_CNT
+ * 3 = CAR_DISTANCE_NEG_CNT
+ * 
+ * This the counterpart of the Android Java classes: Sample.java and SampleType.java.
+ **/
 namespace SedaBackendService.DataObjects
 {
     public class Sample : EntityData
